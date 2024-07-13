ccf785b5054e29e37c14c89a2af68941085230d7
Shivaraj Umadi <shivaumadi@outlook.com>
2024-07-14 03:47:23 +0530

    tweaked react-scripts build

--- frontend/src/components/Error/Nodatafound.jsx
+++ frontend/src/components/Error/Nodatafound.jsx
@@ -3,11 +3,7 @@ import './Error.scss';
 
 export default function Nodatafound() {
   return (
-<<<<<<< HEAD
-    <div class='container-fluid-css'>
-=======
     <div className='container-fluid-css'>
->>>>>>> 885121f (Added filter feature to Beers route)
       <h1>NO DATA FOUND</h1>
     </div>
   );
