3fb211e7fda4057979d3ec19ac95bf00dba1bef0
Shivaraj Umadi <shivaumadi@outlook.com>
2024-10-15 22:28:42 +0530

    removed some bugs with proxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,13 +2,6 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/beer_images',
-    createProxyMiddleware({
-      target: 'https://beerimageloader.s3.ap-south-1.amazonaws.com/',
-      changeOrigin: true,
-    })
-  );
-  app.use(
     '/beer_data',
     createProxyMiddleware({
       target: 'https://jsonbeerdata.s3.ap-south-1.amazonaws.com/',
